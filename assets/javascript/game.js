799151dde518713647eb980328d165ad4705b458
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-02 02:23:18 -0400

    90% done. Missing a few modifications. When key.event pressed only make the alphabet letters appear.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
-var guessesLeft = 9;
-var yourGuesses = [];
+var losses = 0;
+var guessesLeft = 10;
+var yourGuesses = '';
 var computerChoice;
 
 // Target the HTML DOM
@@ -9,45 +10,72 @@ var numOfLosses = document.querySelector('#your-losses');
 var guessesRemaining = document.querySelector('#guesses-left');
 var userGuesses = document.querySelector('#user-guesses');
 
-startGame();
+// startGame();
 
 // initialize the game with a function
-function startGame() {
+// function startGame() {
 
-    // set dococument.onkeyup function
-    document.onkeyup = function(event) {
+// set dococument.onkeyup function
+document.onkeyup = function(event) {
 
-        var userGuess = event.key;
+    // reset();
 
-        // set computerChoice variable as an array;
-        var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var userGuess = event.key;
 
-        // let the computer randomly selects a letter
-        var randomChoice = letters[Math.floor(Math.random() * 26)];
+    // set computerChoice variable as an array;
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-        computerChoice = randomChoice;
-        console.log("computer chose: " + computerChoice); // testing...... IT WORKS! Computer automatically thinks of a random word.
+    // let the computer randomly selects a letter
+    var randomChoice = letters[Math.floor(Math.random() * 26)];
 
-        if (userGuess === computerChoice) {
-            console.log("You picked the right letter as the computer. Letter " + userGuess); // test
+    computerChoice = randomChoice;
+    console.log("computer chose: " + computerChoice); // testing...... IT WORKS! Computer automatically thinks of a random word.
 
-            // alert the user won
-            alert("You Win!");
+    if (userGuess === computerChoice) {
+        console.log("You picked the right letter as the computer. Letter " + userGuess); // test
 
-            // Add +1 to the win score
-            wins++
-            numOfWins.innerHTML = wins;
-        }
+        // alert the user won
+        alert("You Win!");
+
+        // Add +1 to the win score
+        wins++
+        numOfWins.innerHTML = wins;
+
+        // reset the game when user wins
+        reset();
+    }
+
+    if (userGuess !== computerChoice) {
+        console.log("You picked the wrong letter. " + userGuess); // test
+
+        yourGuesses++;
+        userGuesses.textContent = userGuess;
+
+        // reduce the remaining userguess letters
+        guessesLeft--;
+        guessesRemaining.textContent = guessesLeft;
+
+    }
+
+    if (guessesLeft <= 0) {
+        console.log("You Lost!");
+
+        // user lost
+        alert('YOU LOST. HA!');
+
+        // add a win to losses
+        losses++;
+        numOfLosses.textContent = losses;
+
+        // reset the game when user losses
+        reset();
     }
 }
 
-
-
-//         // check if the key wrong key pressed is incorrect
-//         // * if incorrect - increase yourGuesses letters
-//         // * guessesLeft-- - decrease by one
-//         // * if guessesLeft reaches 0 then game over
-//         // add +1 to losses
-
-// 
-//
+function reset() {
+    guessesLeft = 10;
+    yourGuesses = '';
+    guessesRemaining.textContent = '10';
+    userGuesses.textContent = '';
+}
+// }
