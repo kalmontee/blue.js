25624375b7e30f772d8a43cbf37862da5618be7e
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-02 19:44:25 -0400

    New adjustments in the game. Game is currently running well. Need to modify 'guesses so far' and don't output the same letter more than once.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,18 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var yourGuesses = ''; // rename this variable
+var yourGuesses = []; // rename this variable
 var computerChoice; // rename this variable
 
 // Target the HTML DOM
-var numOfWins = document.querySelector('#your-wins');
-var numOfLosses = document.querySelector('#your-losses');
-var guessesRemaining = document.querySelector('#guesses-left'); // rename this variable
-var userGuesses = document.querySelector('#user-guesses');
 
+// var numOfWins = document.querySelector('#your-wins');
+// var numOfLosses = document.querySelector('#your-losses');
+// var userGuesses = document.querySelector('#user-guesses');
+// var guessesRemaining = document.querySelector('#guesses-left'); // rename this variable
 
 // set dococument.onkeyup function
 document.onkeyup = function(event) {
-
-    // reset();
 
     var userGuess = event.key;
 
@@ -35,33 +33,40 @@ document.onkeyup = function(event) {
 
         // Add +1 to the win score
         wins++
-        numOfWins.innerHTML = wins;
+        document.querySelector('#your-wins').textContent = wins;
 
         // reset the game when user wins
         reset();
     }
 
-    if (userGuess !== computerChoice) {
+    // Prevent user from choosing a key that's not part of the alphabet.
+    // a = unicode 65, z = unicode 90. (I learned something new!);
+    if (event.keyCode < 65 || event.keyCode > 90) {
+
+        // alert the user invalid entry
+        alert("Invalid Entry");
+
+    } else if (userGuess !== computerChoice) {
         console.log("You picked the wrong letter. " + userGuess); // test
 
-        yourGuesses++;
-        userGuesses.textContent = userGuess;
+        // use .push() to output userGuess when pressed a letter
+        yourGuesses.push(userGuess + ' ');
+        document.querySelector('#user-guesses').textContent = yourGuesses;
 
         // reduce the remaining userguess letters
         guessesLeft--;
-        guessesRemaining.textContent = guessesLeft;
-
+        document.querySelector('#guesses-left').textContent = guessesLeft;
     }
 
     if (guessesLeft <= 0) {
-        console.log("You Lost!");
-
         // user lost
         alert('YOU LOST. HA!');
 
         // add a win to losses
         losses++;
-        numOfLosses.textContent = losses;
+
+        // numOfLosses.textContent = losses;
+        document.querySelector('#your-losses').textContent = losses;
 
         // reset the game when user losses
         reset();
@@ -69,18 +74,15 @@ document.onkeyup = function(event) {
 }
 
 function reset() {
+    // reset the score to 10
     guessesLeft = 10;
-    yourGuesses = '';
-    guessesRemaining.textContent = '10';
-    userGuesses.textContent = '';
+
+    // reset user guesses letter to nothing
+    yourGuesses = [];
+
+    // reset guesses remaining to 10
+    document.querySelector('#guesses-left').textContent = '10';
+
+    // reset your user guesses letter to nothing.
+    document.querySelector('#user-guesses').textContent = '';
 }
-
-/*
-* TO DO:
-    * Modify your variables.
-        * Change their names (be concise)
-        
-    * event.key - When pressed only alphabet letters should appear in the app.
-    * "Your guesses left" - Store all your pressed letters in the empty content.
-
-*/
